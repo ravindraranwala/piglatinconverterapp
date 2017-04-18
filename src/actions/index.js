8cac852984fd1354210f0ae979b601a3087f3cf0
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-04-18 12:10:45 +0530

    log statements were removed.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,7 @@ import pigLatin from 'piglatin';
 export const PIG_LATIN = 'PIG_LATIN';
 
 export function pigLatinConvert(input){
-  console.log(input);
   const output = pigLatin(`${input.inputtext}`);
-  console.log(output);
   return {
     type: PIG_LATIN,
     payload: output
